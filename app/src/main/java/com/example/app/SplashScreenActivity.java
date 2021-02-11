785d105de4ccb74f89eb74da179cc3bc47a64935
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2021-02-11 12:06:56 +0100

    SplashScreen

---
+++ app/src/main/java/com/example/app/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //redirection vers MainActivity après 3 sec
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //demarage de la page
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        //delay with handler
+        new Handler().postDelayed(runnable, 3000);
+
+    }
+}
