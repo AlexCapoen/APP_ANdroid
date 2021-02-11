15a1d110284e44a297ecbb975981a8bb932c2b57
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2021-02-11 11:28:07 +0100

    start

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -13,35 +13,10 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    Executor executor = Executors.newSingleThreadExecutor();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById((R.id.textView));
-
-        executor.execute(() -> {
-            StarWarsDatabase db = Room.databaseBuilder(this, StarWarsDatabase.class, "database.db").build();
-
-            //add a person
-            Person p = new Person();
-            p.setName("Luke");
-            p.setHairColor(("Blond"));
-            p.setGender("Male");
-            db.personDao().insert(p);
-
-            List<Person> personList = db.personDao().getAll();
-            String text = "";
-            for (Person person : personList){
-                Log.i("ROOM_S2",person.toString());
-                text += person.toString() + "\n";
-            }
-            String finalText = text;
-            runOnUiThread(() -> {
-                textView.setText((finalText));
-            });
-        });
 
     }
 }
