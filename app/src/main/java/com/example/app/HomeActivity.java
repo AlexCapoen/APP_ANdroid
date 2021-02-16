660bfabf4b32ef1aaa33ee34ea1deb70bc581df7
Grosbety <benjamin.grosbety@isen.yncrea.fr>
2021-02-16 16:36:57 +0100

    logout button in HomeActivity

--- app/src/main/java/com/example/app/HomeActivity.java
+++ app/src/main/java/com/example/app/HomeActivity.java
@@ -2,13 +2,33 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button logOutButton;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logOutButton = findViewById(R.id.logOutButton);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToLogin = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intToLogin);
+            }
+        });
     }
 }
